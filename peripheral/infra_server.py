fbbff119779bbeaae3202062d8d848b256ae5cdf
prabhv <19733776+prabhv@users.noreply.github.com>
2019-01-13 18:41:23 +0000

    Update infra_server.py

--- peripheral/infra_server.py
+++ peripheral/infra_server.py
@@ -6,157 +6,250 @@ try:
 except ImportError:
     import gobject as GObject
 
-from Adafruit_LED_Backpack import BicolorMatrix8x8
-
 from bluez_components import *
 
 mainloop = None
 
-COLOR_TABLE = {
-    0: BicolorMatrix8x8.OFF,
-    1: BicolorMatrix8x8.RED,
-    2: BicolorMatrix8x8.GREEN,
-    3: BicolorMatrix8x8.YELLOW
-}
 
-
-def int_to_hex(int_value):
-    return {
-        0: '0',
-        1: '1',
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-        7: '7',
-        8: '8',
-        9: '9',
-        10: 'a',
-        11: 'b',
-        12: 'c',
-        13: 'd',
-        14: 'e',
-        15: 'f'
-    }.get(int_value, '0')
-
-
-def set_display_pixel(display, row, column, value):
-#    display.set_pixel(column, row, COLOR_TABLE[value])
-    x = x + 1
-
-'''
-def set_display_row(row, byte_values):
-
-    pixel_0 = byte_values[0] >> 6
-    pixel_1 = (byte_values[0] & 0x30) >> 4
-    pixel_2 = (byte_values[0] & 0x0c) >> 2
-    pixel_3 = (byte_values[0] & 0x03)
-
-    pixel_4 = byte_values[1] >> 6
-    pixel_5 = (byte_values[1] & 0x30) >> 4
-    pixel_6 = (byte_values[1] & 0x0c) >> 2
-    pixel_7 = (byte_values[1] & 0x03)
-
-    set_display_pixel(display, row, 0, pixel_0)
-    set_display_pixel(display, row, 1, pixel_1)
-    set_display_pixel(display, row, 2, pixel_2)
-    set_display_pixel(display, row, 3, pixel_3)
-    set_display_pixel(display, row, 4, pixel_4)
-    set_display_pixel(display, row, 5, pixel_5)
-    set_display_pixel(display, row, 6, pixel_6)
-    set_display_pixel(display, row, 7, pixel_7)
-#    display.write_display()
-    '''
-
-
-class RowChrc(Characteristic):
-    ROW_UUID = '12345678-1234-5678-1234-56789abc000'
-
-    def __init__(self, bus, index, service, row):
-        Characteristic.__init__(
-            self, bus, index,
-            self.ROW_UUID + int_to_hex(row),  # use the row number to build the UUID
-            ['read', 'write'],
-            service)
-        self.value = [0x00, 0x00]
-        self.row = row
-#        self.display = display
-
-    def ReadValue(self, options):
-        print('RowCharacteristic Read: Row: ' + str(self.row) + ' ' + repr(self.value))
-        return self.value
-
-    def WriteValue(self, value, options):
-        print('RowCharacteristic Write: Row: ' + str(self.row) + ' ' + repr(value))
-#        set_display_row(self.row, value[:2])
-        self.value = value[:2]
 
 ##################CHARACTERISTICS##############################################
 
-##Door##
-##Status can be open value 01 or closed value 00##
+##################DOOR-SERVICE#################################################
+##Status can be open value 'open' or 'close' value
 class DoorChrc(Characteristic):
-    DOOR_UUID = '12345678-1234-5678-1234-56789abc020'
+    DOOR_UUID = '12345678-1234-5678-1234-56789abc1002'
 
     def __init__(self, bus, index, service):
         Characteristic.__init__(
             self, bus, index,
-            self.DOOR_UUID,  # use the row number to build the UUID
-            ['read', 'write', 'notify'],
+            self.DOOR_UUID,  
+            ['read', 'write'],
             service)
-        self.notifying = false
-        self.value = 00
+        self.value = bytes('closed', 'utf-8')
 
     def ReadValue(self, options):
-        print('RowCharacteristic Read: Row: ' + str(self.row) + ' ' + repr(self.value))
+        print('DoorCharacteristic Read:' + repr(self.value))
         return self.value
 
     def WriteValue(self, value, options):
-        print('RowCharacteristic Write: Row: ' + str(self.row) + ' ' + repr(value))
-#        set_display_row(self.row, value[:2])
-        self.value = value[:2]
+        print('DoorCharacteristic Write:' + repr(value))
+        self.value = value
+        
+        
+class DoorStatus(Characteristic):
+    DOOR_STATUS_UUID = '12345678-1234-5678-1234-56789abc1001'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.DOOR_STATUS_UUID,
+                ['read'],
+                service)
+
+    def ReadValue(self, options):
+        # Return Door status as 'faulty' or 'notfaulty'
+        if False:               #door_status == 'fault':
+            return b'fault'
+        else:
+            return b'nofault'
+        
+class DoorService(Service):
+    INFRA_SVC_UUID = '12345678-1234-5678-1234-56789abc1000'
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.INFRA_SVC_UUID, True)
+        self.add_characteristic(DoorChrc(bus, 1, self))
+        self.add_characteristic(DoorStatus(bus, 2, self))
+        
+##################ELEVATOR-SERVICE#################################################
+##Status can be open value 'fault' or 'nofault' value
+class ElevatorStatus(Characteristic):
+    ELEVATOR_STATUS_UUID = '12345678-1234-5678-1234-56789abc2001'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.ELEVATOR_STATUS_UUID,
+                ['read'],
+                service)
+
+    def ReadValue(self, options):
+        # Return Elevator status as 'faulty' or 'notfaulty'
+        if False:               
+            return b'fault'
+        else:
+            return b'nofault'
+
+
+## Return Current  elevator floor number
+class ElevatorCurrentPos(Characteristic):
+    ELEVATOR_CURRENT_UUID = '12345678-1234-5678-1234-56789abc2003'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.ELEVATOR_CURRENT_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('00', 'utf-8')
+
+    def ReadValue(self, options):
+        return self.value
+    
+    
+## Return Current  elevator door status
+class ElevatorDoor(Characteristic):
+    ELEVATOR_DOOR_UUID = '12345678-1234-5678-1234-56789abc2005'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.ELEVATOR_DOOR_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('open', 'utf-8')
+
+    def ReadValue(self, options):
+        return self.value
+    
+    
+
+##Request the elevator to required floor
+
+class ElevatorRequest(Characteristic):
+    ELEVATOR_REQUEST_UUID = '12345678-1234-5678-1234-56789abc2002'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.ELEVATOR_REQUEST_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('00', 'utf-8')
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        self.value = value
+        ##Call elevator request proc, to execute the commands.
+        
+
+##elevator priority request for vehicle to make space in elevator
+## '00' for no priority '01' for priority
+
+class ElevatorPriority(Characteristic):
+    ELEVATOR_PRIORITY_UUID = '12345678-1234-5678-1234-56789abc2004'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.ELEVATOR_PRIORITY_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('00', 'utf-8')
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        self.value = value
+
+
+        
+class ElevatorService(Service):
+    INFRA_SVC_UUID = '12345678-1234-5678-1234-56789abc2000'
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.INFRA_SVC_UUID, True)
+        self.add_characteristic(ElevatorCurrentPos(bus, 2, self))
+        self.add_characteristic(ElevatorRequest(bus, 3, self))
+        self.add_characteristic(ElevatorStatus(bus, 1, self))
+        self.add_characteristic(ElevatorPriority(bus, 4, self))        
+        self.add_characteristic(ElevatorDoor(bus, 5, self))
+
+
+##################PLATFORM-SERVICE#################################################
+
+## Return Current  platform number
+class PlatformNum(Characteristic):
+    PLATFORM_NUM_UUID = '12345678-1234-5678-1234-56789abc3001'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+                self, bus, index,
+                self.PLATFORM_NUM_UUID,
+                ['read'],
+                service)
+        self.value = bytes('01', 'utf-8')
+
+    def ReadValue(self, options):
+        # Return platform number              
+        return value
+
+
+
+##Status can be value '00' for platform in use or 'FF' otherwise
+class PlatformStatus(Characteristic):
+    PLATFORM_STATUS_UUID = '12345678-1234-5678-1234-56789abc3002'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.PLATFORM_STATUS_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('00', 'utf-8')
+
+    def ReadValue(self, options):
+        # Return Elevator status as 'faulty' or 'notfaulty'
+        if False:               
+            return b'fault'
+        else:
+            return b'nofault'
+
+##Next train on the platform
+
+class PlatformInfo(Characteristic):
+    PLATFORM_INFO_UUID = '12345678-1234-5678-1234-56789abc3003'
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.PLATFORM_INFO_UUID,  
+            ['read', 'write'],
+            service)
+        self.value = bytes('TRAIN-DETAILS', 'utf-8')
+
+    def ReadValue(self, options):
+        return self.value
+        
+        
+class PlatformService(Service):
+    INFRA_SVC_UUID = '12345678-1234-5678-1234-56789abc3000'
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.INFRA_SVC_UUID, True)
+        self.add_characteristic(PlatformNum(bus, 1, self))
+        self.add_characteristic(PlatformInfo(bus, 3, self))
+        self.add_characteristic(PlatformStatus(bus, 2, self))
 
 
 ###############################################################################
 
 
 
-
-class LedService(Service):
-    INFRA_SVC_UUID = '12345678-1234-5678-1234-56789abc0010'
-
-    def __init__(self, bus, index):
-        Service.__init__(self, bus, index, self.LED_SVC_UUID, True)
-        self.add_characteristic(RowChrc(bus, 0, self, 0))
-        self.add_characteristic(RowChrc(bus, 1, self, 1))
-        self.add_characteristic(RowChrc(bus, 2, self, 2))
-        self.add_characteristic(RowChrc(bus, 3, self, 3))
-        self.add_characteristic(RowChrc(bus, 4, self, 4))
-        self.add_characteristic(RowChrc(bus, 5, self, 5))
-        self.add_characteristic(RowChrc(bus, 6, self, 6))
-        self.add_characteristic(RowChrc(bus, 7, self, 7))
-
-
 class InfraApplication(Application):
     def __init__(self, bus):  #def __init__(self, bus, display):
         Application.__init__(self, bus)
-        self.add_service(InfraService(bus, 0))
+        self.add_service(ElevatorService(bus, 0))
 
 
-class LedAdvertisement(Advertisement):
+class InfraAdvertisement(Advertisement):
     def __init__(self, bus, index):
         Advertisement.__init__(self, bus, index, 'peripheral')
-        self.add_service_uuid(LedService.LED_SVC_UUID)
+        self.add_service_uuid(ElevatorService.INFRA_SVC_UUID)
         self.include_tx_power = True
-
-
-def setup_display():
-    display = BicolorMatrix8x8.BicolorMatrix8x8()
-    display.begin()
-    display.clear()
-    display.write_display()
-    return display
 
 
 def register_ad_cb():
@@ -202,11 +295,10 @@ def main():
     ad_manager = get_ad_manager(bus)
 
     # Create gatt services
-#    display = setup_display()
     app = InfraApplication(bus)
 
     # Create advertisement
-    test_advertisement = LedAdvertisement(bus, 0)
+    test_advertisement = InfraAdvertisement(bus, 0)
 
     mainloop = GObject.MainLoop()
 
@@ -224,12 +316,9 @@ def main():
         mainloop.run()
     except KeyboardInterrupt:
         x = x + 1
-#        display.clear()
-#        display.write_display()
 
 
 if __name__ == '__main__':
     main()
-
 
 
